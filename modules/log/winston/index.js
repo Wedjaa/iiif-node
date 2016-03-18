1756ec400ab60c9ab87e5458eb23ad381a858504
Fabio 'MrWHO' Torchetti <fabbari@gmail.com>
2016-03-18 13:27:14 +0000

    Create log directory when needed.

--- modules/log/winston/index.js
+++ modules/log/winston/index.js
@@ -1,8 +1,25 @@
+var fs = require('fs');
+var path = require('path');
+
 var winston = require('winston');
+
+function createPath(configuration) {
+  if (typeof configuration.file !== 'undefined'
+	&& typeof configuration.file.filename !== 'undefined') {
+	// It's a file logger, make sure we create the path where we need
+	// to write the logs if it doesn't exist.
+	var logPath = path.normalize(path.dirname(configuration.file.filename));
+	if (!fs.existsSync(logPath)){
+    		fs.mkdirSync(logPath);
+	}
+  }
+}
 
 var WinstonLogger = function(configuration) {
 	for ( var category in configuration ) {
-		winston.loggers.add(category, configuration[category]);
+		var categoryConfig = configuration[category];
+		winston.loggers.add(category, categoryConfig);
+		createPath(categoryConfig);
 	}
 }
 
